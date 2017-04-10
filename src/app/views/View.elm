0ef19976db15a12796b114f8ac9ef35db113b13a
logason <logason@gmail.com>
2017-04-10 19:50:28 +0000

    Setup basic layout for dashboard screen

--- src/app/views/View.elm
+++ src/app/views/View.elm
@@ -1,14 +1,86 @@
 module View exposing (..)
 
 import Action exposing (Msg)
-import Html exposing (Html, div, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, img, text, input)
+import Html.Attributes exposing (src, style, placeholder)
 import Model exposing (Model)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
+    div [ container ]
+        [ div [ content ]
+            [ div [ header ]
+                [ div [ title ] [ text "Dashboard" ]
+                , input [ searchInput, placeholder "Search by service name" ] []
+                ]
+            , div [ pods ]
+                [ div [ pod ]
+                    [ div [ podName ] [ text "booking " ] ]
+                , div [ pod ]
+                    [ div [ podName ] [ text "booking " ] ]
+                , div [ pod ]
+                    [ div [ podName ] [ text "booking " ] ]
+                ]
+            ]
         ]
+
+
+container =
+    style
+        [ ( "padding", "10px" )
+        , ( "background-color", "#FEFEFE" )
+        ]
+
+
+content =
+    style
+        [ ( "max-width", "1100px" )
+        , ( "margin", "0 auto" )
+        ]
+
+
+header =
+    style
+        [ ( "font-size", "24px" )
+        , ( "display", "flex" )
+        , ( "justify-content", "space-between" )
+        , ( "padding", "15px 0" )
+        , ( "margin-top", "10px" )
+        , ( "border-bottom", "1px solid #EFEFEF" )
+        ]
+
+
+title =
+    style [ ( "font-size", "24px" ) ]
+
+
+searchInput =
+    style
+        [ ( "flex-basis", "200px" ) ]
+
+
+pods =
+    style
+        [ ( "display", "flex" )
+        , ( "flex-wrap", "wrap" )
+        , ( "justify-content", "space-between" )
+        , ( "padding", "10px 0" )
+        ]
+
+
+pod =
+    style
+        [ ( "flex-grow", "1" )
+        , ( "padding", "20px" )
+        , ( "margin", "5px" )
+        , ( "border", "1px solid #EFEFEF" )
+        , ( "border-radius", "4px" )
+        , ( "box-shadow", "0 2px 2px rgba(0,0,0,0.02)" )
+        , ( "text-align", "left" )
+        ]
+
+
+podName =
+    style
+        [ ( "font-size", "18px" ) ]
